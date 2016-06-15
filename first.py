5698432c27112feb5545a18d7b221c23c83fc7e5
Jose Gabriel <josripe19@gmail.com>
2016-06-14 20:09:56 -0700

    reordenando el proyecto

--- first.py
+++ first.py
@@ -6,8 +6,22 @@ from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 from PyQt4.QtGui import *
 import numpy as np
-
+import random
+import pprint as pp
 from UI.simulation_widget import Ui_SimulationWindow
+
+
+def load_countries():
+    s = "data" + os.sep + "parsed_countries"
+    d = open(s, 'r')
+    return json.loads(d.read())
+
+
+def load_provinces():
+    s = "data" + os.sep + "parsed_provinces"
+    d = open(s, 'r')
+    return json.loads(d.read())
+
 
 class SimWidget(QMainWindow, Ui_SimulationWindow):
     def __init__(self, parent=None):
@@ -16,10 +30,39 @@ class SimWidget(QMainWindow, Ui_SimulationWindow):
         self.colorEarth = '#009900'
         self.colorWater = '#27b6e9'
 
-        self.load_provinces()
-        self.load_countries()
+        #region Provinces
+        self.name_provinces = [
+            "Pinar del Río",
+            "La Habana",
+            "Matanzas",
+            "Artemisa",
+            "Mayabeque",
+            "Cienfuegos",
+            "Villa Clara",
+            "Sancti Spíritus",
+            "Ciego de Ávila",
+            "Camagüey",
+            "Las Tunas",
+            "Granma",
+            "Holguín",
+            "Santiago de Cuba",
+            "Guantánamo",
+            "Isla de la Juventud"
+        ]
+        #endregion
 
-        self.comboBoxProv.addItems([x for x in self.prov.keys()])
+        self.timer = QTimer()
+        self.time = 500
+        self.running = False
+
+        fig = plt.figure()
+        self.ax = fig.add_subplot(111)
+        self.is_plot = False
+
+        self.sim = Aux()
+
+        self.prov = load_provinces()
+        self.countries = load_countries()
 
         #region Buttons
         self.worldMapBtn.clicked.connect(self.mapa_mundi)
@@ -31,20 +74,60 @@ class SimWidget(QMainWindow, Ui_SimulationWindow):
         self.migrateFromBtn.clicked.connect(self.on_migrate_from_clicked)
         #endregion
 
-    def on_migrate_from_clicked(self):
-        pass
+    def fill_table(self):
+        d = self.sim.tabla1()
+        
 
     def on_stop_clicked(self):
-        pass
+        self.running = False
 
     def on_sim_clicked(self):
-        pass
+        self.running = True
+        self.simulate()
+
+    def simulate(self):
+        if self.running:
+            self.on_next_step_clicked()
+            self.timer.singleShot(self.time, self.simulate)
 
     def on_initial_population_clicked(self):
+        self.iteration = 0
+        self.label.setText("paso:" + str(self.iteration))
+        population = self.sim.initial_population()
+        # pp.pprint(population)
 
+        self.mapa_cuba()
+
+        self.print_population(population)
 
     def on_next_step_clicked(self):
-        pass
+        self.iteration += 1
+        self.label.setText("paso: " + str(self.iteration))
+
+        population = self.sim.step()
+        pp.pprint(population)
+        self.mapa_cuba()
+
+        self.print_population(population)
+
+    def print_population(self, population):
+        lons = []
+        lats = []
+        labels = []
+        for par in population.items():
+            lons.append(self.prov[par[0]][1])
+            lats.append(self.prov[par[0]][0])
+            labels.append(par[1])
+
+        x, y = self.m(lons, lats)
+        # m.plot(x, y, '#000000', markersize=10)
+
+        labels = [x for x in population.values()]
+        for label, xpt, ypt in zip(labels, x, y):
+            plt.text(xpt, ypt, label)
+
+        # plt.show()
+        plt.draw()
 
     def mapa_mundi(self):
         plt.close()
@@ -67,48 +150,53 @@ class SimWidget(QMainWindow, Ui_SimulationWindow):
         for label, xpt, ypt in zip(labels, x, y):
             plt.text(xpt-0.3, ypt+0.1, label, color='b')
 
-        plt.show()
+        # plt.show()
 
     def mapa_cuba(self):
-        plt.close()
+        # plt.close()
+        self.ax.clear()
         c_lon, c_lat = -79.5, 21.5
         delta_lon, delta_lat = 6, 2.5
 
-        m = Basemap(projection='cyl', resolution='i', area_thresh=0.1, lat_0=c_lat, lon_0=c_lon,
+        self.m = Basemap(projection='cyl', resolution='i', area_thresh=0.1, lat_0=c_lat, lon_0=c_lon,
                     llcrnrlat=c_lat-delta_lat, llcrnrlon=c_lon-delta_lon, urcrnrlat=c_lat+delta_lat, urcrnrlon=c_lon+delta_lon)
-        m.drawcoastlines()
-        m.fillcontinents(color=self.colorEarth, lake_color=self.colorWater)
-        m.drawmapboundary(fill_color=self.colorWater)
-        m.drawparallels(np.linspace(c_lat-delta_lat, c_lat+delta_lat, 7), labels=[1, 0, 0, 0], fmt='%.2f')
-        m.drawmeridians(np.linspace(c_lon-delta_lon, c_lon+delta_lon, 9), labels=[0, 0, 1, 0])
+        self.m.drawcoastlines()
+        self.m.fillcontinents(color=self.colorEarth, lake_color=self.colorWater)
+        self.m.drawmapboundary(fill_color=self.colorWater)
+        self.m.drawparallels(np.linspace(c_lat-delta_lat, c_lat+delta_lat, 7), labels=[1, 0, 0, 0], fmt='%.2f')
+        self.m.drawmeridians(np.linspace(c_lon-delta_lon, c_lon+delta_lon, 9), labels=[0, 0, 1, 0])
         lons = [x[1] for x in self.prov.values()]
         lats = [x[0] for x in self.prov.values()]
-        x, y = m(lons, lats)
-        m.plot(x, y, 'bo', markersize=10)
+        x, y = self.m(lons, lats)
+        self.m.plot(x, y, 'bo', markersize=6)
 
         labels = self.prov.keys()
         for label, xpt, ypt in zip(labels, x, y):
-            plt.text(xpt-0.3, ypt+0.1, label, color='w')
+            self.ax.text(xpt-0.3, ypt+0.1, label, color='w')
 
-        plt.show()
+        if not self.is_plot:
+            self.is_plot = True
+            plt.show()
+        # return m
+        # plt.show()
 
-    def load_countries(self):
-        s = "Core" + os.sep + "paises"
-        d = open(s, 'r')
-        self.countries = json.loads(d.read())
-        print("load_countries")
-
-    def load_provinces(self):
-        s = "Core" + os.sep + "provincias"
-        d = open(s, 'r')
-        self.prov = json.loads(d.read())
-        print("load_prov")
 
 class Aux:
     def __init__(self):
         pass
 
     def initial_population(self):
+        prov = load_provinces()
+        d = {}
+        for i in prov.items():
+            d[i[0]] = random.randint(1000, 10000)
+        return d
+
+    def step(self):
+        return self.initial_population()
+
+    def tabla1(self):
+        return {}
 
 
 if __name__ == '__main__':
